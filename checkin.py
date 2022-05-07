892897cd2bc0f2a6618e91691ca03c23812d8312
Founder Yu <62862189+founder-yu@users.noreply.github.com>
2022-05-07 16:44:34 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,4 +1,7 @@
-import requests, json, argparse
+import json
+import logging
+import requests
+import argparse
 
 class CheckIn(object):
     client = requests.Session()
@@ -10,32 +13,31 @@ class CheckIn(object):
         self.password = password
 
     def check_in(self):
-        self.login()
-        headers = {
-            "Host": "w1.v2free.net",
-            "Content-Length": "0",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0",
-            "Referer": "https://w1.v2free.net/user",
-            "Accept-Encoding": "gzip, deflate, br",
-        }
-        response = self.client.post(self.sign_url, headers=headers, timeout=5)
-        print(response.json()["msg"])
-
-    def login(self):
-        print(self.username)
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0",
             "Referer": "https://w1.v2free.net/auth/login",
-            }
+        }
         data = {
             "email": self.username,
             "passwd": self.password,
             "code": "",
-            }
-        response = self.client.post(self.login_url, data=data, headers=headers, timeout=5)
-        print(response.json()["msg"])
+        }
+        self.client.post(self.login_url, data=data, headers=headers)
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/76.0",
+            "Referer": "https://w1.v2free.net/user",
+        }
+        response = self.client.post(self.sign_url, headers=headers)
+
+        logging.info(self.username +" "+ response.json()["msg"])
+
 
 if __name__ == "__main__":
+    LOG_FORMAT = "%(asctime)s\t%(levelname)s\t%(message)s"
+    logging.basicConfig(filename='flow.log',
+                        level=logging.INFO, format=LOG_FORMAT)
+
     parser = argparse.ArgumentParser(description='V2ray签到脚本')
     parser.add_argument('--username', type=str, help='账号')
     parser.add_argument('--password', type=str, help='密码')
